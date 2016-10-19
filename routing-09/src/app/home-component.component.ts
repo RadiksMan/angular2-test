aebdd00d713e106d3775b6e0c253241fe2589c1a
Radiks <radion91@gmail.com>
2016-10-19 18:00:58 +0300

    adds

--- routing-09/src/app/home-component.component.ts
+++ routing-09/src/app/home-component.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { Subscription} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home-component',
@@ -7,14 +9,24 @@ import { Component, OnInit } from '@angular/core';
       Home Component!
     </h1>
     <hr>
+    {{param}}
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy {
+  param: string;
+  private subscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: ActivatedRoute) {
+    this.subscription = router.queryParams.subscribe(
+      queryParam => this.param = queryParam['analytics']
+    )
   }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+
 
 }
